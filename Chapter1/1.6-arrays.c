57c447a6ef7132fada89daebb2bb85d2c85b0f6a
Nate96 <braukhoff96@gmail.com>
2025-03-14 11:08:42 -0500

    learning

---
+++ Chapter1/1.6-arrays.c
@@ -0,0 +1,32 @@
+/* 
+ * Count the number of occurences of each digit, of white space characters
+ * (blank, tab, newline), and all of characters.
+ */
+#include <stdio.h>
+
+/* count digits, white spaces, others */
+int main() 
+{
+   int c, i, nwhite, nother;
+   int ndigit[10];
+
+   nwhite = nother = 0;
+   for (i = 0; i < 10; ++i)
+      ndigit[i] = 0;
+
+   while (c = getchar() != EOF)
+      if ( c >= '0' && c <= '9')
+         ++ndigit[c - '0'];
+      else if (c == ' ' || c == '\n' || c == '\t')
+         ++nwhite;
+      else
+         ++nother;
+
+   printf("digets =");
+   for (i = 0; i < 10; ++i) 
+      printf(" %d", ndigit[i]);
+
+   printf(", white space = %d, other = %d\n", nwhite, nother);
+
+   return 0;
+}
